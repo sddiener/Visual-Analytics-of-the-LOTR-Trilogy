1de2491bf23fcdd5c97d4c5bc92e5951fae91e1e
sddiener <55442757+sddiener@users.noreply.github.com>
2022-01-02 17:13:25 +0000

    Created Network graphs

---
+++ B_LOTR_characterNetwork-iterative.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues Oct 16 23:33:04 2018
+
+@author: Ken Huang
+"""
+
+import spacy
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from afinn import Afinn
+from nltk.tokenize import sent_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def calculate_align_rate(sentence_list):
+    '''
+    Function to calculate the align_rate of the whole novel
+    :param sentence_list: the list of sentence of the whole novel.
+    :return: the align rate of the novel.
+    '''
+    afinn = Afinn()
+    sentiment_score = [afinn.score(x) for x in sentence_list]
+    align_rate = np.sum(sentiment_score)/len(np.nonzero(sentiment_score)[0]) * -2
+
+    return align_rate
+
+
+def calculate_matrix(name_list, sentence_list, align_rate):
+    '''
+    Function to calculate the co-occurrence matrix and sentiment matrix among all the top characters
+    :param name_list: the list of names of the top characters in the novel.
+    :param sentence_list: the list of sentences in the novel.
+    :param align_rate: the sentiment alignment rate to align the sentiment score between characters due to the writing style of
+    the author. Every co-occurrence will lead to an increase or decrease of one unit of align_rate.
+    :return: the co-occurrence matrix and sentiment matrix.
+    '''
+
+    # calculate a sentiment score for each sentence in the novel
+    afinn = Afinn()
+    sentiment_score = [afinn.score(x) for x in sentence_list]
+    # calculate occurrence matrix and sentiment matrix among the top characters
+    # TODO: Why is occurrence_matrix all zeros???? --> Countvectorizer lowers all words. disable this behavior with lowercase=False
+    name_vect = CountVectorizer(vocabulary=name_list, binary=True, lowercase=False)
+    occurrence_each_sentence = name_vect.fit_transform(sentence_list).toarray()
+    cooccurrence_matrix = np.dot(occurrence_each_sentence.T, occurrence_each_sentence)
+    sentiment_matrix = np.dot(occurrence_each_sentence.T, (occurrence_each_sentence.T * sentiment_score).T)
+    sentiment_matrix += align_rate * cooccurrence_matrix
+    cooccurrence_matrix = np.tril(cooccurrence_matrix)
+    sentiment_matrix = np.tril(sentiment_matrix)
+    # diagonals of the matrices are set to be 0 (co-occurrence of name itself is meaningless)
+    shape = cooccurrence_matrix.shape[0]
+    cooccurrence_matrix[[range(shape)], [range(shape)]] = 0
+    sentiment_matrix[[range(shape)], [range(shape)]] = 0
+
+    return cooccurrence_matrix, sentiment_matrix
+
+
+def matrix_to_edge_list(matrix, mode, name_list):
+    '''
+    Function to convert matrix (co-occurrence/sentiment) to edge list of the network graph. It determines the
+    weight and color of the edges in the network graph.
+    :param matrix: co-occurrence matrix or sentiment matrix.
+    :param mode: 'co-occurrence' or 'sentiment'
+    :param name_list: the list of names of the top characters in the novel.
+    :return: the edge list with weight and color param.
+    '''
+    edge_list = []
+    shape = matrix.shape[0]
+    lower_tri_loc = list(zip(*np.where(np.triu(np.ones([shape, shape])) == 0)))
+    normalized_matrix = matrix / np.max(np.abs(matrix))
+    if mode == 'co-occurrence':
+        weight = np.log(2000 * normalized_matrix + 1) * 0.7
+        color = np.log(2000 * normalized_matrix + 1)
+    if mode == 'sentiment':
+        weight = np.log(np.abs(1000 * normalized_matrix) + 1) * 0.7
+        color = 2000 * normalized_matrix
+    for i in lower_tri_loc:
+        edge_list.append((name_list[i[0]], name_list[i[1]], {'weight': weight[i], 'color': color[i]}))
+
+    return edge_list
+
+
+def plot_graph(name_list, name_frequency, matrix, plt_name, mode, path=''):
+    '''
+    Function to plot the network graph (co-occurrence network or sentiment network).
+    :param name_list: the list of top character names in the novel.
+    :param name_frequency: the list containing the frequencies of the top names.
+    :param matrix: co-occurrence matrix or sentiment matrix.
+    :param plt_name: the name of the plot (PNG file) to output.
+    :param mode: 'co-occurrence' or 'sentiment'
+    :param path: the path to output the PNG file.
+    :return: a PNG file of the network graph.
+    '''
+
+    label = {i: i for i in name_list}
+    edge_list = matrix_to_edge_list(matrix, mode, name_list)
+    normalized_frequency = np.array(name_frequency) / np.max(name_frequency)
+
+    plt.figure(figsize=(20, 20))
+    G = nx.Graph()
+    G.add_nodes_from(name_list)
+    G.add_edges_from(edge_list)
+    pos = nx.circular_layout(G)
+    edges = G.edges()
+    weights = [G[u][v]['weight'] for u, v in edges]
+    colors = [G[u][v]['color'] for u, v in edges]
+
+    if mode == 'co-occurrence':
+        nx.draw(G, pos, node_color='#A0CBE2', node_size=np.sqrt(normalized_frequency) * 4000, edge_cmap=plt.cm.Blues,
+                linewidths=10, font_size=35, labels=label, edge_color=colors, with_labels=True, width=weights)
+    elif mode == 'sentiment':
+        nx.draw(G, pos, node_color='#A0CBE2', node_size=np.sqrt(normalized_frequency) * 4000,
+                linewidths=10, font_size=35, labels=label, edge_color=colors, with_labels=True,
+                width=weights, edge_vmin=-1000, edge_vmax=1000)
+    else:
+        raise ValueError("mode should be either 'co-occurrence' or 'sentiment'")
+
+    plt.savefig(path + plt_name + '.png')
+
+
+if __name__ == '__main__':
+    nlp = spacy.load('en_core_web_sm')
+    with open('data/combined_books.txt') as f:
+        novel = f.read()
+    sentence_list = sent_tokenize(novel)
+    align_rate = calculate_align_rate(sentence_list)
+    important_chars = pd.read_csv('data/important_chars.csv')
+    name_list = important_chars['first_name'].tolist()
+    name_frequency = important_chars['mentions_firstname'].tolist()
+    cooccurrence_matrix, sentiment_matrix = calculate_matrix(name_list, sentence_list, align_rate)
+
+    # plot co-occurrence and sentiment graph for Harry Potter
+    plot_graph(name_list, name_frequency, cooccurrence_matrix, 'LOTR co-occurrence graph', 'co-occurrence')
+    plot_graph(name_list, name_frequency, sentiment_matrix, 'LOTR sentiment graph', 'sentiment')
